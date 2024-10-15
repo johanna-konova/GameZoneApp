41a860d4f41c1655bec90559956f44bebc997d77
johanna-konova <j.konova@abv.bg>
2024-10-15 15:45:13 +0300

    Existing game and creator attributes created and implementented

---
+++ GameZoneApp.Web/Attributes/CreatorAttribute.cs
@@ -0,0 +1,41 @@
+﻿using GameZoneApp.Core.Contracts;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+using static GameZoneApp.Web.Attributes.Common.CommonFunctionalities;
+using static GameZoneApp.Core.Constants.MessageTypes;
+using static GameZoneApp.Core.Constants.MessageConstants;
+using GameZoneApp.Web.Controllers;
+
+namespace GameZoneApp.Web.Attributes
+{
+    public class CreatorAttribute : ActionFilterAttribute
+    {
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            Guid gameId = ParseId(context);
+
+            if (gameId != Guid.Empty)
+            {
+                IGameService? gameService =
+                    context.HttpContext.RequestServices.GetService<IGameService>();
+
+                if (gameService == null)
+                {
+                    context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+                }
+
+                var userId = context.HttpContext.User.Id();
+
+                if (await gameService!.IsUserGameCreatorAsync(gameId, userId))
+                {
+                    await next();
+                    return;
+                }
+            }
+
+            HandleError(context, ErrorMessage, MustBeGameCreator, nameof(GameController.All), "Game");
+        }
+    }
+}
---
+++ GameZoneApp.Web/Attributes/ExistingGameAttribute.cs
@@ -0,0 +1,39 @@
+﻿using GameZoneApp.Core.Contracts;
+using GameZoneApp.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+using static GameZoneApp.Core.Constants.MessageConstants;
+using static GameZoneApp.Core.Constants.MessageTypes;
+using static GameZoneApp.Web.Attributes.Common.CommonFunctionalities;
+
+namespace HouseRentingSystem.Web.Attributes
+{
+    public class ExistingGameAttribute : ActionFilterAttribute
+    {
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            Guid gameId = ParseId(context);
+
+            if (gameId != Guid.Empty)
+            {
+                IGameService? gameService =
+                    context.HttpContext.RequestServices.GetService<IGameService>();
+
+                if (gameService == null)
+                {
+                    context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+                    return;
+                }
+
+                if (await gameService.ExistsById(gameId))
+                {
+                    await next();
+                    return;
+                }
+            }
+
+            HandleError(context, ErrorMessage, NonExistentPage, nameof(GameController.All), "Game");
+        }
+    }
+}
